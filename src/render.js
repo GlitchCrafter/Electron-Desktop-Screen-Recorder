e402493edd9211073f909022ecba3588e3a1cf4e
Glitch <109431620+GlitchCrafter@users.noreply.github.com>
2022-07-17 09:27:16 -0400

    buttons
    changes to the render file to reff buttons

---
+++ src/render.js
@@ -0,0 +1,5 @@
+//buttons
+const videelement = document.querySelector('video');
+const startBtn = document.getElementById('startBtn');
+const stopBtn = document.getElementById('stopBtn');
+const videoSelectBtn = document.getElementById('videoSelectBtn');
