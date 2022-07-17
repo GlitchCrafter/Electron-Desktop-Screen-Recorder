29bfef240289a181e5ab14311f93c98baf751be6
Glitch <109431620+GlitchCrafter@users.noreply.github.com>
2022-07-17 09:51:54 -0400

    Update render.js
    Correcting spelling

--- src/render.js
+++ src/render.js
@@ -1,16 +1,27 @@
 //buttons
-const videelement = document.querySelector('video');
+const videoElement = document.querySelector('video');
 const startBtn = document.getElementById('startBtn');
 const stopBtn = document.getElementById('stopBtn');
 const videoSelectBtn = document.getElementById('videoSelectBtn');
 videoSelectBtn.onclick = getVideoSources;
 
-const { desktopCapturer } = require('electron');
+const { desktopCapturer, remote } = require('electron');
+const { Menu } = remote;
 
 //get the available video sources
 async function getVideoSources() {
     const inputsources = await desktopCapturer.getSources({
         types: ['window','screen']
     });
+    const videoOtionMenu = Menu.buildFromTemplate(
+        inputSources.map(source => {
+            return {
+                label: source.name,
+                click: () => selectSource(source)
+            }
+        })
+    );
+
+    videoOptionsMenu.popup();
 
 }
