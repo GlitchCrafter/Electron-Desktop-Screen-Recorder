4c7989653f3419b0e545a987426fb59b3bbd5f06
Glitch <109431620+GlitchCrafter@users.noreply.github.com>
2022-07-17 10:25:29 -0400

    Update render.js
    more changes

--- src/render.js
+++ src/render.js
@@ -10,18 +10,38 @@ const { Menu } = remote;
 
 //get the available video sources
 async function getVideoSources() {
-    const inputsources = await desktopCapturer.getSources({
-        types: ['window','screen']
-    });
-    const videoOtionMenu = Menu.buildFromTemplate(
+    const inputSources = await desktopCapturer.getSources({
+        types: ['window', 'screen']
+      });
+
+    const videoOtionsMenu = Menu.buildFromTemplate(
         inputSources.map(source => {
             return {
                 label: source.name,
                 click: () => selectSource(source)
-            }
+            };
         })
     );
 
     videoOptionsMenu.popup();
 
+}
+
+async function selectSource(source) {
+    videoSelectBtn.innerText = source.name;
+    const constraints = {
+        audio: false,
+        video: {
+            Mandatory: {
+                chromeMediaSource:'desktop',
+                chromeMediaSourceId: source.id
+            }
+        }
+        
+    }
+    //Create a Stream
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    // preview the source in a video element
+    videoElement.srcObject = stream; 
+    videoElement.play();
 }
