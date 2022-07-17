6193dade2acf08784c0b013d4f350fa608bf7a09
Glitch <109431620+GlitchCrafter@users.noreply.github.com>
2022-07-17 09:40:38 -0400

    Video Sources
    Adding const for video sources

--- src/render.js
+++ src/render.js
@@ -3,3 +3,14 @@ const videelement = document.querySelector('video');
 const startBtn = document.getElementById('startBtn');
 const stopBtn = document.getElementById('stopBtn');
 const videoSelectBtn = document.getElementById('videoSelectBtn');
+videoSelectBtn.onclick = getVideoSources;
+
+const { desktopCapturer } = require('electron');
+
+//get the available video sources
+async function getVideoSources() {
+    const inputsources = await desktopCapturer.getSources({
+        types: ['window','screen']
+    });
+
+}
